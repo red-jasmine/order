3df4a3be3b7756c008a53237470003f4c92651bd
liushoukun <liushoukun66@gmail.com>
2024-04-08 23:11:33 +0800

    refactor: 订单发货重构

--- src/Domains/Order/Domain/Models/OrderLogistics.php
+++ src/Domains/Order/Domain/Models/OrderLogistics.php
@@ -3,6 +3,7 @@
 namespace RedJasmine\Order\Domains\Order\Domain\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use RedJasmine\Order\Domains\Common\Domain\Models\HasTradeParties;
 use RedJasmine\Order\Domains\Order\Domain\Enums\Logistics\LogisticsShipperEnum;
