eb33c2afddd8b4936c01e857395cd62e87994ca8
liushoukun <liushoukun66@gmail.com>
2024-05-08 23:27:29 +0800

    refactor: 订单确认

--- src/UI/Http/Buyer/Api/Controller/OrderController.php
+++ src/UI/Http/Buyer/Api/Controller/OrderController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use RedJasmine\Order\Application\Services\OrderCommandService;
 use RedJasmine\Order\Application\Services\OrderQueryService;
 use RedJasmine\Order\Application\UserCases\Commands\OrderCancelCommand;
+use RedJasmine\Order\Application\UserCases\Commands\OrderConfirmCommand;
 use RedJasmine\Order\Application\UserCases\Commands\OrderCreateCommand;
 use RedJasmine\Order\Application\UserCases\Commands\OrderPayingCommand;
 use RedJasmine\Order\Application\UserCases\Commands\Others\OrderHiddenCommand;
@@ -16,8 +17,9 @@ use RedJasmine\Order\UI\Http\Buyer\Api\Resources\OrderResource;
 
 class OrderController extends Controller
 {
-    public function __construct(protected readonly OrderQueryService $queryService,
-                                protected OrderCommandService        $commandService,
+    public function __construct(
+        protected readonly OrderQueryService $queryService,
+        protected OrderCommandService        $commandService,
     )
     {
         $this->commandService->setOperator(function () {
@@ -33,12 +35,14 @@ class OrderController extends Controller
     public function index(Request $request) : AnonymousResourceCollection
     {
         $result = $this->queryService->paginate($request->query());
+
         return OrderResource::collection($result->appends($request->query()));
     }
 
     public function show(Request $request, int $id) : OrderResource
     {
         $result = $this->queryService->find($id, $request->query());
+
         return OrderResource::make($result);
     }
 
@@ -46,8 +50,10 @@ class OrderController extends Controller
     public function store(Request $request) : OrderResource
     {
         $request->offsetSet('buyer', $this->getOwner());
+
         $command = OrderCreateCommand::from($request->all());
         $result  = $this->commandService->create($command);
+
         return OrderResource::make($result);
     }
 
@@ -58,22 +64,24 @@ class OrderController extends Controller
         $command = OrderPayingCommand::from([ 'id' => $order->id, 'amount' => $order->payable_amount ]);
         $payment = $this->commandService->paying($command);
 
-        return static::success([
-                                   'id'            => $order->id,
-                                   'order_payment' => $payment,
-                                   'amount'        => $order->payable_amount->value()
-                               ]);
+        return static::success([ 'id' => $order->id, 'order_payment' => $payment, 'amount' => $order->payable_amount->value() ]);
     }
 
 
-    public function confirm(Request $request)
+    public function confirm(Request $request) : JsonResponse
     {
+        $order = $this->queryService->find($request->id);
 
+        $command = OrderConfirmCommand::from($request->all());
+        $this->commandService->confirm($command);
+
+        return static::success();
     }
 
     public function cancel(Request $request) : JsonResponse
     {
         $this->queryService->find($request->id);
+
         $this->commandService->cancel(OrderCancelCommand::from($request->all()));
 
         return static::success();
@@ -83,8 +91,10 @@ class OrderController extends Controller
     public function destroy($id) : JsonResponse
     {
         $this->queryService->find($id);
+
         $command = OrderHiddenCommand::from([ 'id' => $id ]);
         $this->commandService->buyerHidden($command);
+
         return static::success();
     }
 }
--- tests/UI/Http/Buyer/OrderTest.php
+++ tests/UI/Http/Buyer/OrderTest.php
@@ -20,7 +20,19 @@ class OrderTest extends Base
     }
 
 
-    public function test_can_create_order()
+    /**
+     * @test 能创建订单
+     * 前提条件:
+     * 步骤：
+     *  1、创建订单成功
+     *  2、
+     *  3、
+     * 预期结果:
+     *  1、能查询出订单
+     *  2、订单卖家是当前用户
+     * @return void
+     */
+    public function can_create_order() : void
     {
 
         // 创建订单
@@ -43,7 +55,19 @@ class OrderTest extends Base
     }
 
 
-    public function test_create_after_can_index_and_show() : void
+    /**
+     * @test 创建订单能后 列表 和 详情能查询到
+     * 前提条件: 准备数据
+     * 步骤：
+     *  1、创建订单
+     *  2、查询列表
+     *  3、查询详情
+     * 预期结果:
+     *  1、列表存在订单ID
+     *  2、详情查询成功
+     * @return void
+     */
+    public function create_after_can_index_and_show() : void
     {
 
 
@@ -80,7 +104,17 @@ class OrderTest extends Base
     }
 
 
-    public function test_can_order_cancel() : void
+    /**
+     * @test 创建订单后能取消
+     * 前提条件:准备订单数据
+     * 步骤：
+     *  1、创建订单
+     *  2、取消订单接口
+     * 预期结果:
+     *  1、接口调用成功
+     * @return void
+     */
+    public function can_order_cancel() : void
     {
 
 
@@ -123,7 +157,19 @@ class OrderTest extends Base
 
     }
 
-    public function test_can_order_paying() : void
+    /**
+     * @test 订单能发起支付
+     * 前提条件: 准备订单数据
+     * 步骤：
+     *  1、创建订单
+     *  2、发起支付
+     *  3、
+     * 预期结果:
+     *  1、能获取订单支付单ID
+     *  2、
+     * @return void
+     */
+    public function can_order_paying() : void
     {
 
 
@@ -148,18 +194,22 @@ class OrderTest extends Base
 
     /**
      * @test 能进行订单确认
-     * 前提条件:
+     * 前提条件: 准备订单数据
      * 步骤：
-     *  1、
-     *  2、
-     *  3、
+     *  1、创建订单
+     *  2、发起支付
+     *  3、支付成功
+     *  4、卖家发货
+     *  5、发起确认
      * 预期结果:
-     *  1、
-     *  2、
+     *  1、接口确认成功
+     *  2、订单已确认
      * @return void
      */
-    public function can_order_confirm()
+    public function can_order_confirm() : void
     {
+        // TODO
+
 
     }
 }
