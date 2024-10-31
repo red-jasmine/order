6ab42fa089807f407930ba7ae535b0195eba4e9e
liushoukun <liushoukun66@gmail.com>
2024-10-31 22:46:16 +0800

    refactor(framework): 重构订单发货页面布局并优化多语言支持
    - 调整了订单发货页面的表单布局，将信息列表移至底部
    -优化了表单组件的标签和提示文本，增加了多语言支持- 新增了中文语言包文件，添加了订单发货相关的翻译- 优化了代码格式和命名规范，提高了代码可读性

---
+++ resources/lang/zh_CN/commands.php
@@ -0,0 +1,18 @@
+<?php
+
+
+return [
+
+    'labels'   => [
+        'order'  => '订单',
+        'status' => '状态',
+        'amount' => '金额',
+    ],
+    'shipping' => [
+        'is_finished'          => '是否完成',
+        'products'             => '商品',
+        'is_split'             => '是否拆分发货',
+        'express_company_code' => '快递公司',
+        'express_no'           => '快递单号',
+    ],
+];
